fb5f9323128182168f38d1a81d88cf9ecd135251
qianyu <heqianyuszzaj@126.com>
2017-08-07 00:32:27 +0800

    3
    3

---
+++ create_order/ajax.js
@@ -0,0 +1,51 @@
+// ajax.js
+window.onload = function () {
+	var ad = document.getElementsByClassName('ad');
+	var since = document.getElementsByClassName('since');
+	var circle = document.getElementsByClassName('circle');
+	var time = document.getElementsByClassName('time');
+	var created = document.getElementsByClassName('created');
+	var inputArray = document.getElementsByTagName('input');
+	created[0].onclick = function () {
+		ajax({
+			url: "",
+			type: "post",
+			data: {
+				'name': inputArray[0].value,
+				'tel': inputArray[1].value,
+				'address': inputArray[2].value
+			}
+		})
+	}
+	function ajax(opt) {
+		opt = opt || {};
+	    opt.type = opt.type.toUpperCase() || 'POST';
+	    opt.url = opt.url || '';
+	    opt.async = opt.async || true;
+	    opt.data = opt.data || null;
+	    opt.success = opt.success || function () {};
+	    opt.fail = opt.fail || function () {}
+	    var xhr = null;
+	    if (XMLHttpRequest) {
+	        xhr = new XMLHttpRequest();
+	    }
+	    else {
+	        xhr = new ActiveXObject('Microsoft.XMLHTTP');
+	    }
+	    var params = [];
+	    for (var key in opt.data) {
+	        params.push(key + '=' + opt.data[key]);
+	    }
+	    var postData = params.join('&');
+
+	    if (opt.type === 'POST') {
+	        xhr.open(opt.type, opt.url, opt.async);
+	        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8');
+	        xhr.send(postData);
+	    }
+	    else if (opt.type === 'GET') {
+	        xhr.open(opt.type, opt.url + '?' + postData, opt.async);
+	        xhr.send(null);
+	    }
+	}
+}
--- create_order/main.js
+++ create_order/main.js
@@ -1,1 +1,23 @@
-main.js
+// main.js
+var vm = new Vue({
+	el: '#wrap',
+	data: {
+		guest:{
+			name: '林叶',
+			tel: '13255452123',
+			address: '高新南环路29号留学生创业大厦2201',
+			since: '',
+			circle: '',
+			time: '',
+		},
+		goods:[
+			{goods_name:'燕塘纯牛奶 210ml',quantity:'5'}
+		],
+	},
+	methods:{
+		addgoods() {
+			var n = Object.keys(this.goods).length;
+			Vue.set(this.goods, n, {name:'',age:''})
+		}
+	}
+})
